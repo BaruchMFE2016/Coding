bedfe6e7ceb8d746bd716e2abe046bedeab9ad31
Xinlu Xiao <xinluxiao.baruch@gmail.com>
2016-11-08 12:39:14 -0500

    add the first learning script Python

---
+++ Python_Script/Notes1.py
@@ -0,0 +1,20 @@
+print 'Yankee rule!'
+print 'But not in Boston!'
+print 'Yankee rule,', 'but not in Boston'
+
+print 5//3  # The integer division, it gives the largest integer of the division operator
+print 5**2  # 25, the power operator
+
+pi = 3
+radius = 11
+area = pi*(radius**2)
+print area
+radius = 14
+print 'the new radius is:', radius
+
+x, y = 2, 3
+print x, y
+
+x, y = y, x
+print 'the new x, y would be:', x, y
+
